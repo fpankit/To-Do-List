56584b6b45ac490cd4d7a912071d0d4b0e0dfa27
Students <students@SAITMs-iMac-2.local>
2024-04-30 11:29:30 +0530

    push

---
+++ ourToDoList/ourToDoList/oneViewController.swift
@@ -0,0 +1,29 @@
+//
+//  oneViewController.swift
+//  ourToDoList
+//
+//  Created by Students on 4/30/24.
+//
+
+import UIKit
+
+class oneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
